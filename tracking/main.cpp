7e4d34c4a8c39099b32dd2f238772ea25553843d
Max Curran <curranmax@gmail.com>
2018-07-26 13:45:24 -0400

    Updated tracking

--- tracking/main.cpp
+++ tracking/main.cpp
@@ -14,15 +14,15 @@ int main() {
 	IMU imu(DEFAULT_ADDRESS);
 	sleep(5);
 
-	const float distance = 10; // TxRx distance in meters.
+	const float distance = 1.5; // TxRx distance in meters.
 
-	const int num_samples = 10;
+	const int num_samples = 5;
 	
 
 	// 1)calibrate at no movement condition, collect 1024 sample, average the value as offset value: sstateX // Only perform once before tracking begin
 	float sstateX = 0;
-	int calibration_count = 1024;
-	int max_num_failure = 1024, num_failure = 0;
+	int calibration_count = 8000;
+	int max_num_failure = 8000, num_failure = 0;
 	for(int i = 0; i < calibration_count; ++i) {
 		std::cout << "Calibration iter " << i + 1 << std::endl;
 		auto output = imu.getData();
@@ -49,11 +49,11 @@ int main() {
 	float accX[2] = {0.0, 0.0}; // 1X2 array to hold accelerometer [1] is t [2] is t+1, initialize
 	float positionX[2] = {0.0, 0.0}; // 1X2 array to hold position, initialize 
 	float velocityX[2] = {0.0, 0.0}; // 1X2 array to hold velocity, initialize
-	float t = float(num_samples) / 512.0; // the unit integration time intervel, eg: 512hz t=1/512 second, will change based on number of data took for average.
+	float t = float(num_samples) / 400; // the unit integration time intervel, eg: 512hz t=1/512 second, will change based on number of data took for average.
 
 	int zero_count = 0;
-	const float zero_eps = 0.01;
-	const int zero_count_reset = 25;
+	const float zero_eps = 0.02;
+	const int zero_count_reset = 3;
 
 	while(true) {
 	  auto output = imu.getData(num_samples);
@@ -87,13 +87,46 @@ int main() {
 			// }
 			
 			// 3) reduce data noise by take 64 sample average. (imu.getData(num_samples) automatically gets the requested number of samples and averages them)
-			accX[1] = d.a[0];
-			// accX[1] = accX[1] - sstateX; // apply the zero offset and get ready for filter and integrate
+			//accX[1] = (d.a[0]-sstateX)*9.8;// convect to m/s2
+
+			//*********************************** rotatation matrix recover acceleration to world coordinate***************************************************** 
+			//accX[1] = d.aRaw[0]*9.8;
+			
+			float m[4] = {0.0,0.0,0.0,0.0};
+			float deter = 0.0;
+			m[0] = d.q[0];//w
+			m[1] = d.q[1];//x
+			m[2] = d.q[2];//y
+			m[3] = d.q[3];//z
+			float mRot[3][3] = {{1-2*pow(m[2],2)-2*pow(m[3],2), 2*m[1]*m[2]-2*m[0]*m[3], 2*m[1]*m[3]+2*m[0]*m[2]}, {2*m[1]*m[2]+2*m[0]*m[3], 1-2*pow(m[1],2)-2*pow(m[3],2), 2*m[2]*m[3]-2*m[0]*m[1]}, {2*m[1]*m[3]-2*m[0]*m[2], 2*m[2]*m[3]+2*m[0]*m[1], 1-2*pow(m[1],2)-2*pow(m[2],2)}};// calculate rotation matrix
+			
+			for(int i=0; i<3; i++)
+			  deter = deter + (mRot[0][i]*(mRot[1][(i+1)%3] * mRot[2][(i+2)%3] - mRot[1][(i+2)%3] * mRot[2][(i+1)%3]));
+			
+			float mInv[3][3] = {0};
+			for (int i=0; i<3; i++){
+			  for (int j=0; j<3; j++)
+			    mInv[i][j] = ((mRot[(j+1)%3][(i+1)%3]*mRot[(j+2)%3][(i+2)%3])-(mRot[(j+1)%3][(i+2)%3]*mRot[(j+2)%3][(i+1)%3]))/deter; // calculate inverse matrix
+			  
+			}
+
+			//after get inv matrix we can calculate the world frame acceleration
+			float AccX[3] = {d.linAcc[0], d.linAcc[1], d.linAcc[2]};
+			float accWorld[3] = {0};
+			for (int i=0; i<3; i++){
+			  for (int j=0; j<3; j++){
+			    accWorld[i]+= (mInv[i][j]*AccX[j]*9.8);
+			  }
+			}
+			
+			//*********************************** rotatation matrix recover acceleration to world coordinate*****************************************************
+			accX[1] = accWorld[0];
+			//accX[1] = accX[1] - sstateX*9.8; // apply the zero offset and get ready for filter and integrate
 			
 			// filter out minimum noise using mechanical filter
-			if((accX[1]<=0.005) && (accX[1]>=-0.005)) {
-			    accX[1] = 0.0;
-			}
+			//if((accX[1]<=0.005) && (accX[1]>=-0.005)) {
+			//    accX[1] = 0.0;
+			//}
 
 			if(fabs(accX[1]) < zero_eps) {
 				zero_count++;
@@ -109,7 +142,7 @@ int main() {
 				velocityX[0] = 0.0;
 				velocityX[1] = 0.0;
 			}
-			positionX[1] = positionX[0] + (velocityX[1] + (velocityX[1] - velocityX[0]) / 2.0) * t; // position
+			positionX[1] = positionX[0] + (velocityX[0] + (velocityX[1] - velocityX[0]) / 2.0) * t; // position
 
 			//current data become the initial data of next step
 			accX[0] = accX[1];
@@ -132,6 +165,30 @@ int main() {
 			std::cout << "acceleration: " << accX[1] << std::endl;
 			std::cout << "velocity:     " << velocityX[1] << std::endl;
 			std::cout << "position:     " << positionX[1] << std::endl;
+			std::cout << "sstateX:      " << sstateX*9.8 << " AccXori  " << d.a[0]*9.8 <<std::endl;
+			//std::cout << "raw Acc:      " << d.aRaw[0] << std::endl;
+			std::cout << " X: " << d.r[0] << " Y: " << d.r[1] << " Z: " << d.r[2] << std::endl;
+			std::cout << "Rotation Matrix:    " << d.rotationM[0] << d.rotationM[1] << d.rotationM[2] << std::endl;
+			std::cout << "Rotation Matrix:    " << d.rotationM[3] << d.rotationM[4] << d.rotationM[5] << std::endl;
+			std::cout << "Rotation Matrix:    " << d.rotationM[6] << d.rotationM[7] << d.rotationM[8] << std::endl;
+
+			std::cout << "Det:       " << deter << std::endl;
+
+			for (int i=0; i<3; i++){
+			  std::cout << "Real world frame acc:    " << accWorld[i] << std::endl; 
+			}
+
+			/*point out rotation matrix
+			for (int i=0; i<3; i++){
+			  for(int j=0; j<3; j++){
+			    std::cout << mRot[i][j] ;
+			  }
+			  std::cout << std::endl;
+			}
+			*/
+			
+
+			//std::cout << "lin Acc:      " << d.linAcc[0] << d.linAcc[1] << d.linAcc[2] << std::endl;
 			// std::cout << "r[3]: " << d.r[3] << std::endl; 
 			// std::cout << "Theta: " << theta << std::endl;
 		}
